16d7cf83c096907f9c116f7ac5a6e43e93aa2df4
FHGDev <gideon.lingle14@gmail.com>
2018-05-25 09:34:37 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,36 @@
+const { Client, RichEmbed, Collection } = require('discord.js');
+const bot = new Client();
+
+
+bot.commands = new Collection()
+
+require('fs').readdir("./commands/", (err, files) => {
+  console.log('Loading commands...')
+  if (err) return console.error("Command loading failed!")
+  files.filter(f => f.split(".").pop() == "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`))
+  })
+})
+
+bot.on('ready', () => {
+  console.log("FHGBot ready!")
+  bot.user.setActivity(`for fhg help | ${bot.guilds.size} servers`, {type: "WATCHING"})
+})
+
+bot.on('message', message => {
+  if (!message.guild) return;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  const mArray = message.content.split(" ");
+  const args = mArray.slice(1)
+  const log = mArray[0].slice(prefix.length)
+  const cmd = bot.commands.get(log)
+  
+  if (cmd) {
+    cmd.run(bot, message, args)
+    console.log(`${message.author.username} used the ${log} command.`)
+    // SOON => baselogger(bot, `${message.author.username} used the ${log} command.`, bot.user.avatarURL)
+  }
+})
+
+bot.login(process.env.token)
