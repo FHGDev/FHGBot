15d4fb77ca0343e6049b06b79d2ac6dcc92e8ae3
FHGDev <gideon.lingle1@gmail.com>
2018-12-27 09:53:58 -0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,10 @@
-module.exports.run = (bot, message, args, em) => {
-  em
+module.exports.run = (bot, message, args) => {
+ const em = new (require('discord.js').RichEmbed)()
   .setTitle("FHGBot Help")
   .setDescription("List of commands")
   .addField("Help Commands", "help", true)
   .addField("Bot Commands", "ping", true)
+  .addField("Emergency Commands", "alert", true)
   .setTimestamp()
   .setColor("RANDOM")
   
