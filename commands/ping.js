0bce7c2a254b56e51ac7b6839e65cac2c36af5a8
FHGDev <gideon.lingle1@gmail.com>
2018-12-27 10:44:04 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,6 @@
-module.exports.run = (bot, message, args, em) => {
+module.exports.run = (bot, message, args) => {
   const ping = Math.floor(bot.ping)
+  
   const em = new (require('discord.js').RichEmbed)()
   .setTitle("FHGBot Ping")
   .setDescription(`PONG! My ping is ${ping}ms.`)
