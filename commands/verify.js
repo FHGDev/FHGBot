764f45256bf5cc17a3ce2ff44f6b81508ee54820
Gideon Lingle <gideon.lingle1@gmail.com>
2019-01-18 13:05:46 -0600

    Create verify.js

---
+++ commands/verify.js
@@ -0,0 +1,21 @@
+module.exports = {
+  help: {
+    name: "verify"
+  }, 
+  run: (bot, message, args) => {
+    const v = message.guild.roles.find(r => r.name == "Verified")
+    const m = message.guild.roles.find(r => r.name == "Members")
+    const ch = message.guild.channels.find(c => c.id == "535894520932466697")
+    const em = new (require('discord.js').RichEmbed)()
+    .addField("FHGBot Verify", `You are now verified!")
+    .setTimestamp()
+    .setColor("GREEN")
+    .setFooter(`${message.member.displayName} just verified.`)
+    message.member.addRoles([v.id, m.id])
+    message.channel.send({embed: em}).then(m => {
+      m.delete(1000)
+      message.delete(1000)
+    })
+    ch.send(`${message.member.displayName} just verified.`)
+  }
+}
